6e03f93f846b3207e8618f35a1a901f1548d5e36
shark121 <boakyes175@gmail.com>
2023-11-07 21:50:09 +0000

    updated config regex

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions:["/^((?!api).)*$/gm"],
+  pageExtensions:["/^((?!requestApi.api.js).)*$/gm"],
 
 
 }
